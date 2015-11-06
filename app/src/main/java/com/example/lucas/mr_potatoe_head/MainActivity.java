40ff31e7464bd749b73dd3592413d2dc9ca932a4
Lucas-Jollie <lucas_jollie@outlook.com>
2015-11-06 13:39:53 +0100

    Removed non-functional settings and obsolete checkboxes

--- app/src/main/java/com/example/lucas/mr_potatoe_head/MainActivity.java
+++ app/src/main/java/com/example/lucas/mr_potatoe_head/MainActivity.java
@@ -21,8 +21,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         // initialise all components
         nose = (ImageView) findViewById(R.id.imageView8);
